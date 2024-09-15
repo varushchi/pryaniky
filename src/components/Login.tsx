5bbf2b5bc656c87048502be4d4fc1d8f53b51d61
varushchi <vadim.shchigolev#mail.ru>
2024-09-15 18:57:31 +0300

    styled login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './Login.css'
 
 interface Props{
   handleToken: (token: string) => void
@@ -7,6 +11,15 @@ interface Props{
 export default function Login(props: Props) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [usernameError, setUsernameError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const usernameRef = useRef(null)
+  const passwordRef = useRef(null)
+  const [usernameFocus, setUsernameFocus] = useState(false)
+  const [passwordFocus, setPasswordFocus] = useState(false)
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const HOST = 'https://test.v5.pryaniky.com'
     async function getToken(username: string, password: string,){
@@ -27,22 +40,87 @@ export default function Login(props: Props) {
   {
     const userReg = new RegExp(/user\d+$/)
     const passwordReg = new RegExp(/password$/)
+
     if (userReg.test(username) && passwordReg.test(password)){
       getToken(username, password)
     }
-    else{
-      alert('wrong username or password')
+    else if (!userReg.test(username) && !passwordReg.test(password)){
+      setUsernameError(true)
+      setPasswordError(true)
     }
-
+    else if (!userReg.test(username)){
+      setUsernameError(true)
+    }
+    else if (!passwordReg.test(password)){
+      setPasswordError(true)
+    }
   }
+
+  useEffect(() => {
+    if (!usernameFocus && username && !(new RegExp(/user\d+$/).test(username))){
+      setUsernameError(true)
+    }
+    if(usernameFocus){
+      setUsernameError(false)
+    }
+  }, [usernameFocus])
+
+  useEffect(() => {
+    if (!passwordFocus && password && !(new RegExp(/password$/).test(password))){
+      setPasswordError(true)
+    }
+    if(passwordFocus){
+      setPasswordError(false)
+    }
+  }, [passwordFocus])
+
+
 
   return (
     <div className='Login'>
-      <form onSubmit={(e) => {e.preventDefault()}}>
-        <input type='text' name='user' autoComplete='username' value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
-        <input type='password' name='password' autoComplete='current-password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-        <button onClick={()=>{handleClick()}}>Login</button>
-      </form>
+      <Box
+        component="form"
+        sx = {{
+          width: '300px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '15px'
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="username"
+          label="Username"
+          ref={usernameRef}
+          onFocus={() => setUsernameFocus(true)}
+          onBlur={() => setUsernameFocus(false)}
+          error={usernameError}
+          helperText={usernameError && "Incorrect username"}
+          variant="outlined" 
+          value={username}
+          onChange={(e)=>{setUsername(e.target.value)}}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          ref={passwordRef}
+          onFocus={() => setPasswordFocus(true)}
+          onBlur={() => setPasswordFocus(false)}
+          error={passwordError}
+          helperText={passwordError && "Incorrect password"}
+          type='password'
+          variant="outlined"
+          value={password}
+          onChange={(e)=>{setPassword(e.target.value)}}
+        />
+        <Button
+          variant="contained"
+          size="medium"
+          onClick={()=>{handleClick()}}
+          sx = {{height: '56px'}}
+        >Login</Button>
+      </Box>
     </div>
   )
 }
